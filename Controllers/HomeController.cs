e5c3bcd5f1e73fba8f64eb65efa3a814f55ae021
Rudra Pal <Rudra Pal@DESKTOP-EPCRK2D>
2024-08-08 10:32:59 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Ajaxsignup.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebGrease.Css.Extensions;
+
+namespace Ajaxsignup.Controllers
+{
+    public class HomeController : Controller
+    {
+        RegistrationEntities db = new RegistrationEntities();
+
+
+        public ActionResult Index()
+        {
+            var data = db.Signups.ToList();
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string q)
+        {
+            if (string.IsNullOrEmpty(q) ==  false)
+            {
+                List<Signup> s =  db.Signups.Where(model => model.firstname.StartsWith(q)).ToList();
+                return PartialView("_searchdata",s);
+            }
+            else
+            {
+                List<Signup> s = db.Signups.ToList();
+                return PartialView("_searchdata", s);
+            }
+            
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Signup p)
+        {
+            if (ModelState.IsValid) 
+            {
+                db.Signups.Add(p);
+               int a = db.SaveChanges();
+                if (a > 0) 
+                {
+                    return Json("Data Inserted");
+                }
+                else
+                {
+                    return Json("Data not Insterted");
+                }
+
+            }
+            return View();
+        }
+
+    }
+}
